ccae6837f609c0ffb145aa408c70f2f935883c3b
Ilya <bound1ess.dev@gmail.com>
2014-12-19 21:22:40 +0600

    Setup SASS compilation & minification.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require("gulp");
 var util = require("gulp-util");
+var sass = require("gulp-ruby-sass");
+var minifyCSS = require("gulp-minify-css");
 
-gulp.task("default", function() {
-	util.log("Hello, world!");	
+gulp.task("sass", function() {
+	util.log("Compiling and minifying SASS files...");
+	gulp.src("assets/sass/main.sass")
+		.pipe(sass({"sourcemap=none": true, quiet: true, noCache: true}))
+		.pipe(minifyCSS({}))
+		.pipe(gulp.dest("public/css"));
+	util.log("Done.");
+});
+
+gulp.task("default", ["sass"], function() {
+	util.log("All done, good sir.");	
 });
