7e832d6f9f1af1e514e25cf4924c0f23d326497f
Ilya <bound1ess.dev@gmail.com>
2014-12-20 17:42:05 +0600

    Add two useful helpers: isValidJSON and parseJSON.

--- assets/js/_helpers.js
+++ assets/js/_helpers.js
@@ -4,6 +4,15 @@ var helpers = (function($) {
 		return Array.isArray(smth);
 	};
 
+	this.parseJSON = function(string) {
+		return JSON.parse(string);
+	};
+
+	this.isValidJSON = function(string) {
+		try { JSON.parse(string) } catch (e) { return false; }
+		return true;
+	};
+
 	return this;
 
 })();
--- spec/javascripts/helpers_spec.js
+++ spec/javascripts/helpers_spec.js
@@ -3,4 +3,11 @@ describe("Helpers suite", function() {
 		expect(helpers.isArray([])).toBe(true);
 		expect(helpers.isArray({})).toBe(false);
 	});
+	it("has a method that parses JSON for you", function() {
+		expect(helpers.parseJSON("{\"key\": 123}").key).toBe(123);
+	});
+	it("has a method that validates JSON", function() {
+		expect(helpers.isValidJSON("meaningless string")).toBe(false);
+		expect(helpers.isValidJSON("{}")).toBe(true);
+	});
 });
